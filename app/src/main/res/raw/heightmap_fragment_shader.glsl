a346557a1623c3fda4b02303670df4dcaf1bba10
Zion <ZionLife1025@163.com>
2017-12-13 15:58:29 +0800

    <Update>更新着色器代码

--- app/src/main/res/raw/heightmap_fragment_shader.glsl
+++ app/src/main/res/raw/heightmap_fragment_shader.glsl
@@ -1,7 +1,14 @@
 #version 120
 precision mediump float;
-varying vec3 v_Color;
-
-void main() {
-    gl_FragColor = vec4(v_Color, 1.0);
+varying vec2 vTextureCorrd; //接收从顶点着色器过来的参数
+uniform sampler2D sTexture; //纹理内容数据
+void main(){
+    //给此片元从纹理中采样出颜色值
+    gl_FragColor = texture2D(sTexture, vTextureCorrd);
 }
+//precision mediump float;
+//varying vec3 v_Color;
+//
+//void main() {
+//    gl_FragColor = vec4(v_Color, 1.0);
+//}
--- app/src/main/res/raw/heightmap_vertex_shader.glsl
+++ app/src/main/res/raw/heightmap_vertex_shader.glsl
@@ -1,13 +1,22 @@
 #version 120
-uniform mat4 u_Matrix;
-attribute vec3 a_Position;
-varying vec3 v_Color;
+uniform mat4 uMVPMatrix; //总变换矩阵
+attribute vec3 aPosition; //顶点位置
+attribute vec2 aTexCoor; //顶点纹理坐标
+varying vec2 vTextureCoord; //用于传递给片元着色器的变量
 
-void main() {
-    v_Color = mix(vec3(0.180, 0.467,0.153),
-                  vec3(0.660, 0.670, 0.680),
-                  a_Position.y
-                );
-
-    gl_Position = u_Matrix * vec4(a_Position, 1.0);
+void main(){
+    gl_Position = uMVPMatrix * vec4(aPosition, 1); //根据总变换矩阵计算此次绘制顶点的位置
+    vTextureCoord = aTexCoor; //将接收的纹理坐标传递给片元着色器
 }
+//uniform mat4 u_Matrix;
+//attribute vec3 a_Position;
+//varying vec3 v_Color;
+//
+//void main() {
+//    v_Color = mix(vec3(0.180, 0.467,0.153),
+//                  vec3(0.660, 0.670, 0.680),
+//                  a_Position.y
+//                );
+//
+//    gl_Position = u_Matrix * vec4(a_Position, 1.0);
+//}
