ac927e161c23e8132986f2e9c43fe30dc8ce0129
Zion <ZionLife1025@163.com>
2017-12-13 15:27:26 +0800

    <Add>创建顶点数据缓冲区VertexBuffer与索引缓冲区IndexBuffer

---
+++ app/src/main/java/com/zionstudio/seabedmodelling/IndexBuffer.java
@@ -0,0 +1,59 @@
+package com.zionstudio.seabedmodelling;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import static android.opengl.GLES20.GL_ARRAY_BUFFER;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_STATIC_DRAW;
+import static android.opengl.GLES20.glBindBuffer;
+import static android.opengl.GLES20.glBufferData;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGenBuffers;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static com.zionstudio.seabedmodelling.Constants.BYTES_PER_FLOAT;
+
+/**
+ * Created by QiuXi'an on 2017/12/13 0013.
+ * Email Zionlife1025@163.com
+ */
+
+public class IndexBuffer {
+    private int bufferId;
+
+    public IndexBuffer(float[] vertexData) {
+        //Allocate a buffer.
+        final int buffers[] = new int[1];
+        glGenBuffers(buffers.length, buffers, 0);
+        if (buffers[0] == 0) {
+            throw new RuntimeException("Could not create a new vertex buffer object.");
+        }
+        bufferId = buffers[0];
+
+        //Bind to the buffer.
+        glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+
+        //Transfer data to native memory.
+        FloatBuffer vertexArray = ByteBuffer
+                .allocateDirect(vertexData.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(vertexData);
+        vertexArray.position();
+
+        //Transfer data from native memory to the GPU buffer.
+        glBufferData(GL_ARRAY_BUFFER, vertexArray.capacity() * BYTES_PER_FLOAT,
+                vertexArray, GL_STATIC_DRAW);
+
+        //Unbind the buffer when we are done with it.
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    public void setVertexAttribPointer(int dataOffset, int attributeLocation, int componentCount, int stride) {
+        glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+        glVertexAttribPointer(attributeLocation, componentCount, GL_FLOAT, false, stride, dataOffset);
+        glEnableVertexAttribArray(attributeLocation);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+}
